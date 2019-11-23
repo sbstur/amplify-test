db43af4c95b915f92e8ba384fff481202d0a0b9e
sbstur <sbstur@yahoo.com>
2019-11-22 23:15:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn AWS
+          Learn KBTG AWS
         </a>
       </header>
     </div>
